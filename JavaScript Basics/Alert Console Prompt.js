6fcaff66674dbf397cdfd6457de00d848455f473
Shambhavi Sahay <41591299+iamshambhavi@users.noreply.github.com>
2020-09-07 22:13:29 +0530

    Create Alert Console Prompt.js

---
+++ JavaScript Basics/Alert Console Prompt.js
@@ -0,0 +1,9 @@
+let firstName=prompt("What is your first name?");
+let lastName=prompt("What is your last name?");
+let age=prompt("What is your age?");
+alert("Your full name is "+firstName+" "+lastName);
+alert("Your age is "+age);
+console.log("Your full name is "+firstName+" "+lastName);
+console.log("Your age is "+age);
+let days=age*365.25;
+alert("You have lived this much days= "+days);
